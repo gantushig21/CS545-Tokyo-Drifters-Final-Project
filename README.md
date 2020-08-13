a2b2e6d83d31a91cc64518e2ebab80f5b9882bb2
Gantushig Bayasgalan <gantushig21@gmail.com>
2020-08-12 20:45:22 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CS545-Tokyo-Drifters-Final-Project
+
+## Description
+Web Application Architecture course project. Our application is built for car rental services. They register all cars in the system which are rented to customers. In other words, when a customer needs a car in city, they just came to the car rental company and the service worker checks out the car for customer. After using the car, customer returns it as the way he or she got it from a service worker.
+
+## Technologies
+Java Spring-MVC, JSP, HTML, CSS, JSP, Ajax, JavaScript.  
--- src/main/resources/sql/admins.sql
+++ src/main/resources/sql/admins.sql
@@ -1,7 +1,10 @@
-INSERT INTO credentials(username,password,enabled) VALUES ('gantushig','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
-INSERT INTO credentials(username,password,enabled) VALUES ('seegii','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
-INSERT INTO credentials(username,password,enabled) VALUES ('dat','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
+INSERT INTO credentials(username,password,enabled) VALUES ('admin','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
+INSERT INTO authority (id,username, authority) VALUES (1,'admin', 'ROLE_ADMIN');
 
-INSERT INTO authority (id,username, authority) VALUES (1,'gantushig', 'ROLE_ADMIN');
-INSERT INTO authority (id,username, authority) VALUES (2,'seegii', 'ROLE_ADMIN');
-INSERT INTO authority (id,username, authority) VALUES (3,'dat', 'ROLE_ADMIN');
+-- INSERT INTO credentials(username,password,enabled) VALUES ('gantushig','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
+-- INSERT INTO credentials(username,password,enabled) VALUES ('seegii','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
+-- INSERT INTO credentials(username,password,enabled) VALUES ('dat','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
+--
+-- INSERT INTO authority (id,username, authority) VALUES (1,'gantushig', 'ROLE_ADMIN');
+-- INSERT INTO authority (id,username, authority) VALUES (2,'seegii', 'ROLE_ADMIN');
+-- INSERT INTO authority (id,username, authority) VALUES (3,'dat', 'ROLE_ADMIN');
